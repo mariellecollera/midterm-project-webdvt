cbf961696866492ea46f33fccc45bb7089da7497
mariellecollera <marielle_anne_collera@dlsl.edu.ph>
2026-09-03 13:44:04 +0800

    refactor: replace Dashboard with Home in routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import AddTransaction from './pages/AddTransaction';
-import TransactionDetail from './pages/TransactionDetail';
-import Summary from './pages/Summary';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddTransaction from "./pages/AddTransaction";
+import TransactionDetail from "./pages/TransactionDetail";
+import Summary from "./pages/Summary";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Home />} />
       <Route path="/add" element={<AddTransaction />} />
       <Route path="/transaction/:id" element={<TransactionDetail />} />
       <Route path="/summary" element={<Summary />} />
-      <Route path="*" element={<Dashboard />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }

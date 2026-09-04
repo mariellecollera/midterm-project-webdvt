b8e4bc4fd27e2eacc8f687e8b9e4ebe0ada11aa0
mariellecollera <marielle_anne_collera@dlsl.edu.ph>
2026-09-04 23:16:49 +0800

    chore: configure jsx-uses-vars rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
@@ -17,12 +18,15 @@ export default [
       },
     },
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
